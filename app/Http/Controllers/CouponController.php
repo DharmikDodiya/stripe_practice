21699928d6ab4ba3c8a5ccbf768991710a51cbba
dharmik <dharmikd@zignuts.com>
2023-05-29 18:50:44 +0530

    create new couponController and perform create , update , get by id and list all coupon and delete coupon funcationality

---
+++ app/Http/Controllers/CouponController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CouponController extends Controller
+{
+    public function createCoupon(Request $request){
+
+        $coupon = $this->stripe->coupons->create([
+        'percent_off' => $request->percent_off,
+        'name'      => $request->name,
+        'duration' => 'repeating',
+        'duration_in_months' => $request->duration,
+        ]);
+        if(isset($coupon)){
+            return success('Coupon Created Succssfully',$coupon);
+        }
+            return error('Coupon Not Created');
+    }
+
+    public function getCoupon($id){
+        $coupon = $this->stripe->coupons->retrieve($id, []);
+        if(isset($coupon)){
+            return success('Coupon Details',$coupon);
+        }
+            return error('Coupon Not Found');
+    }
+
+    public function updateCoupon($id,Request $request){
+
+        $coupon = $this->stripe->coupons->update(
+        $id,
+        ['metadata' => ['order_id' => '6735','name' => $request->name, 'percent_off' => $request->percent_off ,  'duration_in_months' => $request->duration]]
+        );
+        if(isset($coupon)){
+            return success('Coupon Updated Successfully',$coupon);
+        }
+            return error('Coupon Not Updated');
+    }
+
+    public function listCoupon(){
+        $coupon = $this->stripe->coupons->all();
+        if($coupon){
+            return success('Coupon List',$coupon);
+        }
+            return error('Coupon Not Found');
+    }
+
+    public function deleteCoupon($id){
+        $this->stripe->coupons->delete($id, []);
+        return success('Coupon deleted Successfully');
+    }
+
+    // public function createPayout(Request $request){
+    //     //dd('dd');
+    //     $payout = $this->stripe->payouts->create([
+    //     'amount' => 1200,
+    //     'currency' => 'usd',
+    //     ]);
+    //     return success('PayOut Created Successfully',$payout);
+    // }
+}
