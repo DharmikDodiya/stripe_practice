a9e77a95ad6d6738906dab8be4492324f4e087c3
dharmik <dharmikd@zignuts.com>
2023-05-29 18:49:33 +0530

    create new PaymentIntentController perfrom create , update , get by id , list all paymentIntent and capture PaymentIntent

---
+++ app/Http/Controllers/PaymentIntentController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentIntentController extends Controller
+{
+    public function createFile(Request $request){
+
+        $paymentIntent = $this->stripe->paymentIntents->create([
+        'amount' => $request->amount,
+        'currency' => 'usd',
+        'automatic_payment_methods' => [
+            'enabled' => true,
+        ],
+        ]);
+        return success('PaymentIntent Created Successfully',$paymentIntent);
+    }
+
+    public function getPaymentIntent($id){
+        $paymentIntent = $this->stripe->paymentIntents->retrieve(
+        $id,
+        []
+        );
+        return success('Payment-Intent Details',$paymentIntent);
+    }
+
+    public function listPaymentIntent(){
+        $paymentIntents = $this->stripe->paymentIntents->all();
+        if(count($paymentIntents)){
+            return success('Payment Intent List',$paymentIntents);
+        }
+            return error('Payment-Intent List Not Found');
+    }
+
+    public function updatePaymentIntent(Request $request,$id){
+    //dd($request->amount);
+        $paymentIntent = $this->stripe->paymentIntents->update(
+        $id,
+        ['metadata' => ['order_id' => '6735' , 'amount' => $request->amount]]
+        );
+        if(isset($paymentIntent)){
+            return success('PaymentIntent Updated Successfully',$paymentIntent);
+        }
+        return error('PaymentIntent Not Updated');
+    }
+
+    public function capturePaymentIntent($id){
+        $paymentIntent = $this->stripe->paymentIntents->capture(
+        $id,
+        []
+        );
+        if(isset($paymentIntent)){
+            return success('PaymentIntent Capture Successfully',$paymentIntent);
+        }
+        return error('PaymentIntent Not Captured');
+    }
+}
