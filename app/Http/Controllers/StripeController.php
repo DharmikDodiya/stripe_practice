0060f372493fa26f5303d672713d7f82878a399c
dharmik <dharmikd@zignuts.com>
2023-05-29 18:47:12 +0530

    changes into StripeController remove commented code and changes into stripePayment method

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -123,10 +123,8 @@ class StripeController extends Controller{
         return success('Payment Created Successfully',$data);
     }
 
-
     public function stripePayment(Request $request){
         $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
-
         $user = Auth::user();
 
         $customer = $stripe->customers->create([
@@ -134,14 +132,7 @@ class StripeController extends Controller{
             'email' => 'sarman@gmail.com',
             'description' => 'My First Test Customer (created for API docs)',
           ]);
-        //dd($customer->id);
-        // $charge = $stripe->charges->create([
-        // 'amount' => 300,
-        // 'currency' => 'usd',
-        // 'source' => $customer->token,
-        // 'description' => 'book',
-        // 'customer' => $customer,
-        // ]);
+
         $paymentMethod = $this->stripe->paymentMethods->create([
             'type' => 'card',
             'card' => [
@@ -179,36 +170,6 @@ class StripeController extends Controller{
           ]);
 
         return success('charge created ',$paymentIntent);
-
-    // try{
-    //   $token =$this->stripe->tokens->create([
-    //         'card' => [
-    //             'number' => $request->number,
-    //             'exp_month' => $request->exp_month,
-    //             'exp_year' => $request->exp_year,
-    //             'cvc' => $request->cvc,
-    //         ],
-    //     ]);
-    //     \Stripe\Stripe::setApiKey('sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic');
-    //     $charge = \Stripe\Charge::create([
-    //         'amount' => 999,
-    //         'currency' => 'usd',
-    //         'description' => 'Example charge',
-    //         'source' => $token->id,
-    //       ]);
-
-    //   //dd($token);
-    //   $response = $this->stripe->paymentIntents->create([
-    //     'amount' => 500,
-    //     'currency' => 'gbp',
-    //     'source'    => $token->id,
-    //     'description' => 'first_payment',
-    //   ]);
-    //     return response()->json($charge);
-    // }
-    // catch(Expectation $ex){
-    //     return response()->json('error');
-    // }
     }
 }
 
@@ -235,138 +196,3 @@ class StripeController extends Controller{
 
 
 
-    // public function form(){
-    //     return view('stripe.form');
-    // }
-
-    // public function makePayment(Request $request){
-    //     $data = $request->all();
-    //     //dd($data);
-
-    //     Stripe::setApiKey('sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic');
-    //     $charge = \Stripe\Charge::create([
-    //         'source' => $request->stripeToken,
-    //         'description' => "10 cucumbers from Roger's Farm",
-    //         'amount' => 2000,
-    //         'currency' => 'usd',
-    //       ]);
-    //       dd($charge);
-    // }
-
-    // public function makePayment(Request $request)
-    // {
-    //     $data = $request->all();
-
-    //     Stripe::setApiKey('sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic');
-
-    //     try {
-    //         $charge = \Stripe\Charge::create([
-    //             'source' => $request->stripeToken,
-    //             'description' => "10 cucumbers from Roger's Farm",
-    //             'amount' => 2000,
-    //             'currency' => 'usd',
-    //         ]);
-
-    //         dd($charge);
-    //     } catch (\Stripe\Exception\CardException $e) {
-    //         // Handle the exception for failed payments
-    //         dd($e->getMessage());
-    //     }
-    // }
-
-
-    // public function stripe()
-    // {
-    //     return view('stripe');
-    // }
-
-    // public function stripePost(Request $request)
-    // {
-    //     Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-
-    //     $data = Stripe\Charge::create ([
-    //             "amount" => 100 * 100,
-    //             "currency" => "inr",
-    //             "source" => $request->stripeToken,
-    //             "description" => "Test payment"
-    //     ]);
-
-    //     dd($data);
-    //     Session::flash('success', 'Payment successful!');
-
-    //     return back();
-    // }
-
-
-
-    // }catch(Exception $ex){
-    //     return response()->json([['response'=> 'error']],500);
-    // }
-        //dd($request->all());
-        // try{
-          //   $stripe = new Stripe\StripeClient(env('STRIPE_SECRET'));
-          // //  dd($stripe);
-          //   $res = $stripe->tokens->create([
-          //       'card' =>[
-          //           'number'        => $request->number,
-          //           'exp_month'     => $request->exp_month,
-          //           'exp_year'      => $request->exp_year,
-          //           'cvc'           => $request->cvc,
-          //       ],
-          //   ]);
-
-          //   dd($res);
-            // Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-            // //dd($res);
-            // $response = $stripe->charges->create([
-            //     'amount'        => $request->amount,
-            //     'currency'      => 'usd',
-            //     'source'        => $res->id,
-            //     'description'   => $request->description,
-            // ]);
-
-            //new code
-          //  $stripe = new \Stripe\StripeClient(
-          //       'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-          //     );
-          //     $customer =  $stripe->customers->create([
-          //       'description' => 'My First Test Customer (created for API docs at https://www.stripe.com/docs/api)',
-          //     ]);
-
-          //     dd($customer->id);
-          //   $card = $stripe = new \Stripe\StripeClient(
-          //       'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-          //     );
-          //     $stripe->customers->createSource(
-          //       'cus_9s6XWPuHZWFcfK',
-          //       ['source' => 'tok_visa']
-          //     );
-
-          //   $stripe = new \Stripe\StripeClient(
-          //       'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-          //     );
-          //     $response=  $stripe->charges->create([
-          //       'amount' => 2000,
-          //       'currency' => 'inr',
-          //       'source' =>  $res->id,
-          //       'description' => 'My First Test Charge (created for API docs at https://www.stripe.com/docs/api)',
-          //     ]);
-
-          //   $stripe = new \Stripe\StripeClient(
-          //       'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-          //     );
-          //   $response = $stripe->paymentIntents->create([
-          //       'amount' => 2000,
-          //       'currency' => 'usd',
-          //       'automatic_payment_methods' => [
-          //         'enabled' => true,
-          //       ],
-          //     ]);
-
-          //   //   dd($response);
-          //   return $response;
-          //   return response()->json([$response],201);
-
-        // }catch(Exception $ex){
-        //     return response()->json([['response' => 'Error']],500);
-        // }
