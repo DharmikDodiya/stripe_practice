fe9aef64c3674954f4ba5d8511805ded79d97683
dharmik <dharmikd@zignuts.com>
2023-05-29 18:44:35 +0530

    work on card module get card , delete card and list of all card funcationality

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 class CardController extends Controller
 {
-
     public function createCard(Request $request,$id){
         $card = $this->stripe->customers->createSource(
             $id,
@@ -15,4 +14,32 @@ class CardController extends Controller
         return success('Card Created Successfully',$card);
     }
 
+    public function getCard($cus_id,$card_id){
+        $card = $this->stripe->customers->retrieveSource(
+          $cus_id,
+          $card_id,
+          []
+        );
+        return success('Card details',$card);
+    }
+
+    public function deleteCard($cus_id,$card_id){
+        $card = $this->stripe->customers->deleteSource(
+        $cus_id,
+        $card_id,
+        []
+        );
+        return success('Card Deleted Successfully');
+    }
+
+    public function listCard($id){
+        $cards = $this->stripe->customers->allSources(
+            $id,
+            [
+                'object' => 'card',
+                'limit' => 3,
+            ]
+        );
+        return success('Card List',$cards);
+    }
 }
