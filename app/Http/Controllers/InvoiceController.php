699619397dc76b4c563f27ffbe0806f622862937
dharmik <dharmikd@zignuts.com>
2023-05-30 19:10:30 +0530

    changes into invoiceController use stripe secret key and get daynamic data

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -9,10 +9,8 @@ use Illuminate\Http\Request;
 class InvoiceController extends Controller
 {
     public function createInvoice(Request $request){
-        $stripe = new \Stripe\StripeClient(
-            'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-        );
-        $invoice = $stripe->invoices->create([
+
+        $invoice = $this->stripe->invoices->create([
             'customer' => $request->customer_id,
             'description' => $request->description,
             'discounts'   => $request->discount,
@@ -36,11 +34,8 @@ class InvoiceController extends Controller
     }
 
     public function getInvoice($id){
-        $stripe = new \Stripe\StripeClient(
-            'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-        );
         try{
-        $invoice = $stripe->invoices->retrieve(
+        $invoice = $this->stripe->invoices->retrieve(
         $id,
             []
         );
@@ -51,11 +46,8 @@ class InvoiceController extends Controller
     }
 
     public function updateInvoice(Request $request,$id){
-        $stripe = new \Stripe\StripeClient(
-            'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-        );
         try{
-        $invoice = $stripe->invoices->update(
+        $invoice = $this->stripe->invoices->update(
             $id,
             ['metadata' => ['order_id' => '6735', 'customer' => $request->customer_id,
             'description' => $request->description]]
@@ -69,11 +61,8 @@ class InvoiceController extends Controller
     }
 
     public function listInvoice(){
-        $stripe = new \Stripe\StripeClient(
-            'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-        );
         try{
-        $invoice = $stripe->invoices->all();
+        $invoice = $this->stripe->invoices->all();
             return success('Invoice List',$invoice);
         }catch(Exception $ex){
             return error('Invoice Not Found',$ex);
@@ -81,13 +70,10 @@ class InvoiceController extends Controller
     }
 
     public function deleteInvoice($id){
-        $stripe = new \Stripe\StripeClient(
-            'sk_test_51N9PI1SA4SjjlNffXOm2HtQ2zzoiol7xYb5YOZo0ifzWyk81AsLmUiM4vkL2SgbbcJ4WRNhrB4gxYRWIAvx1gB6j00pZlqtqic'
-          );
             $invoice = Invoice::where('invoice_id',$id)->first();
             if(isset($invoice)){
                 $invoice->delete();
-                $invoice = $stripe->invoices->delete(
+                $invoice = $this->stripe->invoices->delete(
                 $id,
                 []
             );
